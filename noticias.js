f380527e2c287c3faa01de45f280865b95fa2119
Alex <alex.pinheiro@compasso.com.br>
2019-06-29 09:05:57 -0300

    Construção pagina teste

--- noticias.js
+++ noticias.js
@@ -3,7 +3,16 @@ var http = require('http');
 var server = http.createServer( function(req, res){
 
     var categoria = req.url;//recuperando a requisição url
-    res.end("<html><body>Portal de noticias</body></html>");
+
+    if(categoria == '/tecnologia'){
+        es.end("<html><body>Portal de tecnologia</body></html>");
+    } else if (categoria == '/moda') {
+        es.end("<html><body>Portal de moda</body></html>");
+    } else if (categoria == '/beleza') {
+        es.end("<html><body>Portal de beleza</body></html>");
+    } else {
+        es.end("<html><body>Portal de noticias</body></html>");
+    }
 
 });
 
