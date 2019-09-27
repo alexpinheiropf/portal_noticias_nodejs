790bc591730040a7f62d047acd739a077ee24336
Alex Pinheiro <alex.pinheiro@compasso.com.br>
2019-09-27 11:07:46 -0300

    ajuste node

--- noticias.js
+++ noticias.js
@@ -6,7 +6,7 @@ var server = http.createServer( function(req, res){
 
     if(categoria == '/tecnologia'){
         es.end("<html><body>Portal de tecnologia</body></html>");
-    } else if (categoria == '/moda') {
+    } else if (categoria == '/moda1') {
         es.end("<html><body>Portal de moda</body></html>");
     } else if (categoria == '/beleza') {
         es.end("<html><body>Portal de beleza</body></html>");
